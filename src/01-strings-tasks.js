3558e284373af3a86d1f13baa2ca983a31be20f0
ElderDragonfly <ElderDragonfly@yandex.ru>
2024-10-26 22:32:45 +0300

    more tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -110,8 +110,8 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   'A', 5  => 'AAAAA'
  *   'cat', 3 => 'catcatcat'
  */
-function repeatString(value, count) {
-  return [value, count];
+function repeatString(/* value, count */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -126,8 +126,8 @@ function repeatString(value, count) {
  *   'I like legends', 'end' => 'I like legs',
  *   'ABABAB','BA' => 'ABAB'
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  return str.split('').splice(str.indexOf(value), value.length).join('');
 }
 
 /**
